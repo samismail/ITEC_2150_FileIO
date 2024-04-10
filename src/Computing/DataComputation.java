c1cdc6e4a3e5862e76fa04474cdc7c8abb31c10b
samismail <sismail1@ggc.edu>
2024-04-10 11:25:39 -0400

    4/10 in class

--- src/Computing/DataComputation.java
+++ src/Computing/DataComputation.java
@@ -1,56 +1,75 @@
 package Computing;
 
+import java.io.*;
+
+class Statistics{
+    double sum;
+    double lowest;
+    double highest;
+    double average;
+
+    public Statistics(double sum, double lowest, double highest, double average) {
+        this.sum = sum;
+        this.lowest = lowest;
+        this.highest = highest;
+        this.average = average;
+    }
+}
+
 public class DataComputation {
 
-    class Statistics{
-        double sum;
-        double lowest;
-        double highest;
-        double average;
-        public Statistics(double sum, double lowest, double highest, double average) {
-            this.sum = sum;
-            this.lowest = lowest;
-            this.highest = highest;
-            this.average = average;
-        }
+    public static void main(String[] args) {
 
-        public double getSum() {
-            return sum;
-        }
-
-        public void setSum(double sum) {
-            this.sum = sum;
-        }
-
-        public double getLowest() {
-            return lowest;
-        }
-
-        public void setLowest(double lowest) {
-            this.lowest = lowest;
-        }
-
-        public double getHighest() {
-            return highest;
-        }
-
-        public void setHighest(double highest) {
-            this.highest = highest;
-        }
-
-        public double getAverage() {
-            return average;
-        }
-
-        public void setAverage(double average) {
-            this.average = average;
-        }
+        Statistics stats = descriptiveStat("src/Computing/data.txt");
+        String output = String.format("///Output\\\\\\ \nSum: %f\nAverage: %.2f\nHighest:%.1f\nLowest: %.1f ", stats.sum,stats.average,stats.highest,stats.lowest);
+        writeFile("src/Computing/outputdata.txt",output);
+        display(output);
     }
 
-   public class DataComputation{
+    public static Statistics descriptiveStat(String fileName){
+        double sum = 0;
+        double lowest = 0;
+        double highest = 0;
+        int count = 0;
+        try(BufferedReader br = new BufferedReader(new FileReader(fileName))){
+            String line ;
+            if((line = br.readLine()) != null){
+                String[] dataList = line.split(",");
+                for(String number: dataList){
+                    double num = Double.parseDouble(number.trim());
+                    sum += num;
+                    count++;
+                    if(num < lowest){
+                        lowest =  num;
+                    }
 
+                    if(num > highest){
+                        highest = num;
+                    }
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        double average  = sum / count;
+        return new Statistics(sum, lowest, highest, average);
     }
 
 
+    public static void writeFile(String outputFileName, String content){
 
+        try(FileWriter fw = new FileWriter(outputFileName);
+            BufferedWriter bw = new BufferedWriter(fw);)
+        {
+            bw.write(content);
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void display(String content){
+        System.out.println(content);
+    }
 }
